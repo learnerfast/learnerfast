0543c5437e37074c39654c805ee94a64c45201f3
Chayank Kashyap <chayankkashyap@Chayanks-MacBook-Air.local>
2025-10-20 11:54:38 +0530

    Add toast notification for email confirmation and update Supabase credentials

--- src/components/register/index.jsx
+++ src/components/register/index.jsx
@@ -1,10 +1,12 @@
 'use client';
 import HeaderSix from "@/layout/headers/header-6"; 
 import RegisterArea from "./register-area";
+import { Toaster } from "react-hot-toast";
 
 const Register = () => {
   return (
     <>
+      <Toaster position="top-center" />
       <HeaderSix />
       <RegisterArea />
     </>
