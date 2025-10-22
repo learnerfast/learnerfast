cdc95558f719f2017898ad55c5fef907e693db16
Chayank Kashyap <chayankkashyap@Chayanks-MacBook-Air.local>
2025-10-22 08:07:39 +0530

    Add toast notifications to sign-in page and smooth page transitions

--- src/components/sign-in/index.jsx
+++ src/components/sign-in/index.jsx
@@ -1,10 +1,12 @@
 'use client';
 import HeaderSix from "@/layout/headers/header-6"; 
 import SingnInArea from "./singn-in-area";
+import { Toaster } from 'react-hot-toast';
 
 const SignIn = () => {
   return (
     <>
+      <Toaster position="top-center" toastOptions={{ style: { zIndex: 9999 } }} />
       <HeaderSix hideNav={true} />
       <SingnInArea />
     </>
