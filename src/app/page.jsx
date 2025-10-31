fdf3b37658986a665054ea32a4bc3d7fe51c0650
learnerfast <your-email@example.com>
2025-10-31 22:05:00 +0530

    Set all page titles to LearnerFast

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import Wrapper from "@/layouts/wrapper";
 import HomeOne from "@/components/homes/home";
 
 export const metadata = {
-  title: "LearnerFast - Data analytics"
+  title: "LearnerFast"
 };
 
 const HomePage = () => {
