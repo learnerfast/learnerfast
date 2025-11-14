c91de385ad1a62fb22b8020dd9865d2e7c4cb1f6
chhayank kashyap <support@learnerfast.com>
2025-11-14 11:43:13 +0530

    Fix mobile menu login/register links

--- src/layout/headers/mobile-menus.jsx
+++ src/layout/headers/mobile-menus.jsx
@@ -54,10 +54,10 @@ const MobileMenus = () => {
             </React.Fragment>
           ))}
           <li style={{ marginTop: "20px", paddingTop: "20px", borderTop: "1px solid #e5e7eb" }}>
-            <Link href="/signin" style={{ display: "block", padding: "10px 0", color: "#6366f1", fontWeight: "500" }}>Login</Link>
+            <Link href="/sign-in" style={{ display: "block", padding: "10px 0", color: "#6366f1", fontWeight: "500" }}>Login</Link>
           </li>
           <li>
-            <Link href="/signup" style={{ display: "block", padding: "10px 15px", backgroundColor: "#6366f1", color: "white", borderRadius: "8px", textAlign: "center", fontWeight: "500" }}>Register</Link>
+            <Link href="/register" style={{ display: "block", padding: "10px 15px", backgroundColor: "#6366f1", color: "white", borderRadius: "8px", textAlign: "center", fontWeight: "500" }}>Register</Link>
           </li>
         </ul>
       </nav>
