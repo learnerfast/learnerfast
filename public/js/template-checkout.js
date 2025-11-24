646c68a09322d0f016a33c7df14222998d26d8f2
chhayank kashyap <support@learnerfast.com>
2025-11-24 07:41:51 +0530

    Fix template checkout payment API routing

--- public/js/template-checkout.js
+++ public/js/template-checkout.js
@@ -70,7 +70,11 @@
           }
           
           try {
-            const response = await fetch('https://www.learnerfast.com/api/payment/initiate', {
+            const apiUrl = window.location.hostname.includes('learnerfast.com') 
+              ? '/api/payment/initiate'
+              : 'https://www.learnerfast.com/api/payment/initiate';
+            
+            const response = await fetch(apiUrl, {
               method: 'POST',
               headers: { 'Content-Type': 'application/json' },
               body: JSON.stringify({
